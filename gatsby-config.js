c6fdb3d8aa87cedf954b52c3cb3664f5ef7d9bbe
HyunSeob <sytte1017@gmail.com>
2021-03-01 13:21:07 +0900

    fix: rss url fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,8 +85,10 @@ module.exports = {
                   ...edge.node.frontmatter,
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  url: `${site.siteMetadata.siteUrl}/${encodeURIComponent(
+                    edge.node.fields.slug
+                  )}`,
+                  guid: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
                   custom_elements: [{ "content:encoded": edge.node.html }],
                 };
               });
